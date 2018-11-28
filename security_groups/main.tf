cd9fc1546ea9bce34dc2f4ec531a3a6ef0c6d602
Aviad Shikloshi <aviad.shikloshi@similarweb.com>
2018-11-28 07:03:47 +0200

    adding more details rules in map

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -22,7 +22,7 @@ variable consul_security_group_tcp_rules {
   default = [8300, 8301, 8400, 8500]
 }
 
-resource aws_default_security_group consul_security_group {
+resource aws_default_security_group consul_security_group_tcp {
   dynamic "ingress" {
     for_each = var.consul_security_group_tcp_rules
     iterator = rule
@@ -32,6 +32,36 @@ resource aws_default_security_group consul_security_group {
       to_port = rule.value
       cidr_blocks = [ "0.0.0.0/0" ]
     }
+  }
+}
 
+variable consul_security_group_rules {
+  type = list(object({
+    from_port = number
+    to_port = number
+    protocol = string
+    cidr_blocks = list(string)
+  }))
+
+  default = [
+    {
+      from_port = 8300
+      to_port = 8300
+      protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
+    }
+  ]
+}
+
+resource aws_default_security_group consul_security_group {
+  dynamic "ingress" {
+    for_each = var.consul_security_group_rules
+    iterator = rule
+    content {
+      protocol = rule.value.protocol
+      from_port = rule.value.from_port
+      to_port = rule.value.to_port
+      cidr_blocks = rule.value.cidr_blocks
+    }
   }
 }
