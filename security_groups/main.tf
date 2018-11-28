2b679bb31877fb6c4293da4722c6bbacf5752fa7
Aviad Shikloshi <aviad.shikloshi@similarweb.com>
2018-11-28 07:05:43 +0200

    split to files

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -1,27 +1,3 @@
-variable aws_access_key {
-  type = string
-}
-
-variable aws_secret_key {
-  type = string
-}
-
-variable aws_region {
-  type = string
-  default = "us-west-2"
-}
-
-provider aws {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = var.aws_region
-}
-
-variable consul_security_group_tcp_rules {
-  type = list
-  default = [8300, 8301, 8400, 8500]
-}
-
 resource aws_default_security_group consul_security_group_tcp {
   dynamic "ingress" {
     for_each = var.consul_security_group_tcp_rules
@@ -33,24 +9,6 @@ resource aws_default_security_group consul_security_group_tcp {
       cidr_blocks = [ "0.0.0.0/0" ]
     }
   }
-}
-
-variable consul_security_group_rules {
-  type = list(object({
-    from_port = number
-    to_port = number
-    protocol = string
-    cidr_blocks = list(string)
-  }))
-
-  default = [
-    {
-      from_port = 8300
-      to_port = 8300
-      protocol = "tcp"
-      cidr_blocks = [ "0.0.0.0/0" ]
-    }
-  ]
 }
 
 resource aws_default_security_group consul_security_group {
---
+++ security_groups/variables.tf
@@ -0,0 +1,13 @@
+variable aws_access_key {
+  type = string
+}
+
+variable aws_secret_key {
+  type = string
+}
+
+variable aws_region {
+  type = string
+  default = "us-west-2"
+}
+
