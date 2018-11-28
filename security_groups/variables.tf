01ea7cfb393a92e9c490673e4d509241f09b5cc1
Aviad Shikloshi <aviad.shikloshi@similarweb.com>
2018-11-28 07:15:11 +0200

    remove default value from consul security group rules

--- security_groups/variables.tf
+++ security_groups/variables.tf
@@ -23,12 +23,4 @@ variable consul_security_group_rules {
     protocol = string	
     cidr_blocks = list(string)	
   }))	
-   default = [	
-    {	
-      from_port = 8300	
-      to_port = 8300	
-      protocol = "tcp"	
-      cidr_blocks = [ "0.0.0.0/0" ]	
-    }	
-  ]	
 }	
